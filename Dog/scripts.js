9f002c62c0306e35dab38fb01135abc1ddef526a
SethTims <sethtims13@gmail.com>
2022-07-08 14:21:01 +0100

    mroe dog

--- Dog/scripts.js
+++ Dog/scripts.js
@@ -1,7 +1,7 @@
 const app = document.getElementById('root')
 
 const logo = document.createElement('img')
-logo.src = ''
+logo.src = 'dog.png'
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
@@ -10,21 +10,20 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://dog.ceo/api/breed/Borzoi/images/random', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(dog => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
       const h1 = document.createElement('h1')
-      h1.textContent = movie.title
+      h1.textContent = dog.title
 
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+      const img = document.createElement('img')
+      img.src = "https://dog.ceo/api/breed/Borzoi/images/random"
 
       container.appendChild(card)
       card.appendChild(h1)
